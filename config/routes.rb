24a55b4d4bb584839ce0c0cf663860d2b0387b3b
SrNascimento40 <wallacediogo10@hotmail.com>
2025-03-18 20:16:29 -0300

    feat: user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :create]
   resources :legal_cases do
     resources :change_logs, only: [:index, :create]
     resources :messages, only: [:index, :create]
