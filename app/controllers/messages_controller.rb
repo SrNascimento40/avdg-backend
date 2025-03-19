e5bee084509f91528158c707c1ae206ac2609feb
SrNascimento40 <wallacediogo10@hotmail.com>
2025-03-19 10:04:42 -0300

    feat: message feature routes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,48 @@
 class MessagesController < ApplicationController
+    def index
+        @messages = Message.all
+        render json: @messages
+    end
+
+    def show
+        @message = Message.find(params[:id])
+        render json: @message
+    end
+
+    def create
+        legal_case = LegalCase.find(params[:legal_case_id])
+        current_user = 1 # gamb pra rodar, depois mudar para current_user.id
+        receiver_id = current_user != legal_case.client_id ? legal_case.client_id : legal_case.lawyer_id
+        @message = Message.new(message_params.merge(
+            sender_id: current_user,
+            receiver_id: receiver_id,
+            legal_case_id: legal_case.id
+        ))
+        if @message.save
+            render json: @message, status: :created
+        else
+            render json: @message.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @message = Message.find(params[:id])
+        if @message.update(message_params)
+            render json: @message
+        else
+            render json: @message.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @message = Message.find(params[:id])
+        @message.destroy
+        head :no_content
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:content)
+    end
 end
--- db/migrate/20250318213152_create_messages.rb
+++ db/migrate/20250318213152_create_messages.rb
@@ -1,11 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[8.0]
   def change
     create_table :messages do |t|
-      t.references :sender, null: false, foreign_key: { to_table: :users } # Aponta para a tabela "users"
-      t.references :receiver, null: false, foreign_key: { to_table: :users } # Também aponta para "users"
+      t.references :sender, null: false, foreign_key: { to_table: :users }
+      t.references :receiver, null: false, foreign_key: { to_table: :users }
       t.references :legal_case, null: false, foreign_key: true
-      t.text :message
-      t.boolean :read, default: false # Adicionei um valor padrão aqui, caso seja útil
+      t.text :content, null: false
+      t.boolean :read, default: false 
 
       t.timestamps
     end
