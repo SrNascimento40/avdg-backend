013864499254d6103857c6dc51600246a731af3c
SrNascimento40 <wallacediogo10@hotmail.com>
2025-07-16 12:36:44 -0300

    feat: payments back-end

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,2 +1,42 @@
 class InvoicesController < ApplicationController
-end
+    def index
+      render json: Invoice.all
+    end
+  
+    def show
+      invoice = Invoice.find(params[:id])
+      render json: invoice
+    end
+  
+    def create
+      byebug
+      invoice = Invoice.new(invoice_params)
+      if invoice.save
+        render json: invoice, status: :created
+      else
+        render json: invoice.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      invoice = Invoice.find(params[:id])
+      if invoice.update(invoice_params)
+        render json: invoice
+      else
+        render json: invoice.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      invoice = Invoice.find(params[:id])
+      invoice.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def invoice_params
+      params.require(:invoice).permit!
+    end
+  end
+  
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,21 @@
 class PaymentsController < ApplicationController
-end
+
+    
+    def create
+      invoice = Invoice.find(params[:invoice_id])
+      payment = invoice.payments.new(payment_params)
+  
+      if payment.save
+        render json: payment, status: :created
+      else
+        render json: payment.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def payment_params
+      params.require(:payment).permit(:amount, :payment_date, :method)
+    end
+  end
+  
